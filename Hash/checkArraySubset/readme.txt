0c7466e5c80345001b2dcd01576be4ef6b5c0641
Rakshith Kunchum <settyblue@gmail.com>
2016-06-08 00:15:40 -0700

    Solution to check if array A is subset of array B.

---
+++ Hash/checkArraySubset/readme.txt
@@ -0,0 +1,13 @@
+/**
+ * http://www.geeksforgeeks.org/find-whether-an-array-is-subset-of-another-array-set-1/
+ * Find whether an array is subset of another array | Added Method 3
+ * Given two arrays: arr1[0..m-1] and arr2[0..n-1]. 
+ * Find whether arr2[] is a subset of arr1[] or not. Both the arrays are not in sorted order. 
+ * It may be assumed that elements in both array are distinct.
+ */
+
+ Solution 1:
+ -----------
+ Implemented using Hashset.
+ Time Complexity = O(m+n)
+ Drawback of this method is that it doesnt account if the array has repeated elements.
---
+++ Hash/checkArraySubset/src/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * http://www.geeksforgeeks.org/find-whether-an-array-is-subset-of-another-array-set-1/
+ * Find whether an array is subset of another array | Added Method 3
+ * Given two arrays: arr1[0..m-1] and arr2[0..n-1]. 
+ * Find whether arr2[] is a subset of arr1[] or not. Both the arrays are not in sorted order. 
+ * It may be assumed that elements in both array are distinct.
+ */
+
+/**
+ * @author rakshith
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Integer[] array1 = {11, 1, 13, 21, 3, 7};
+		Integer[] array2 = {11, 3, 8, 1};
+		if(checkSubset(array1,array2)){
+			System.out.println("subset yes.");
+		}else{
+			System.out.println("subset no.");
+		}
+	}
+
+	private static boolean checkSubset(Integer[] array1, Integer[] array2) {
+		Integer[] s=array1,b=array2;
+		
+		if(array1.length > array2.length){
+			s=array2;b=array1;
+		}
+		Set<Integer> set = new HashSet<Integer>(Arrays.asList(b));
+		for(int i=0; i<s.length; i++){
+			if(!set.contains(s[i])){
+				return false;
+			}
+		}
+		return true;
+	}
+
+}
