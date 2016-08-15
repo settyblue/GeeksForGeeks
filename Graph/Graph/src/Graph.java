8933c6293a8b544464c769c41de7ab5ba6d1f0b1
Rakshith Kunchum <settyblue@gmail.com>
2016-08-15 13:33:22 -0700

    Added main to test graph class.

--- Graph/Graph/src/Graph.java
+++ Graph/Graph/src/Graph.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 /**
  * http://www.geeksforgeeks.org/depth-first-traversal-for-a-graph/
  * 
@@ -8,10 +9,55 @@
  *
  */
 public class Graph {
-
-	public static void main(String[] args) {
-		
-		
+	
+	private int vertices;
+	private int edges;
+	private ArrayList<ArrayList<Integer>> adjList;
+	
+	Graph(int vertices, int edges){
+		this.vertices = vertices;
+		this.edges = edges;
+		adjList = new ArrayList<>();
+		for(int i=0; i<vertices; i++){
+			adjList.set(i, new ArrayList<Integer>()) ;
+		}
+	}
+	
+	public void addEdge(int v1, int v2){
+		this.getAdjList().get(v1).add(v2);
+		this.edges++;
+	}
+	
+	public void addSymmetricEdge(int v1, int v2){
+		this.getAdjList().get(v1).add(v2);
+		this.getAdjList().get(v2).add(v1);
+		this.edges += 2;
 	}
 
+	public ArrayList<ArrayList<Integer>> getAdjList() {
+		return adjList;
+	}
+
+	public int getVertices() {
+		return vertices;
+	}
+
+	public void setVertices(int vertices) {
+		this.vertices = vertices;
+	}
+
+	public int getEdges() {
+		return edges;
+	}
+
+	public void setEdges(int edges) {
+		this.edges = edges;
+	}
+
+	public void DFS(int startVertex) {
+		boolean[] visited = new boolean[this.vertices];
+		
+	}
+	
+	
 }
---
+++ Graph/testGraph/src/Main.java
@@ -0,0 +1,28 @@
+/**
+ * http://www.geeksforgeeks.org/depth-first-traversal-for-a-graph/
+ */
+
+/**
+ * @author rakshith
+ *
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Graph g = new Graph(4,0);
+		 
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+ 
+        System.out.println("Following is Depth First Traversal");
+ 
+        g.DFS(0);
+
+	}
+
+}
