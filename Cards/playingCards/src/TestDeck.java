3e5c7407572474ef6be4930a06a228c24db81018
Rakshith Kunchum <settyblue@gmail.com>
2016-10-01 01:09:43 -0700

    Added class to test functioning of deck class.

---
+++ Cards/playingCards/src/TestDeck.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+
+/**
+ * @author rakshith
+ *
+ */
+public class TestDeck {
+
+	public static void main(String[] args) {
+		StandardDeck deck = new StandardDeck();
+		deck.printDeck();
+	}
+
+}
