cf3e192ab8548ed60fb383bd5411150bf38f18e3
Rakshith Kunchum <settyblue@gmail.com>
2017-01-09 19:01:00 +0100

    Solution to find max sum pairs.

--- Algorithms/DynamicProgramming/maxSumPairs/src/Main.java
+++ Algorithms/DynamicProgramming/maxSumPairs/src/Main.java
@@ -1,7 +1,11 @@
 import java.util.Arrays;
 import java.lang.Math;
+
 /**
  * http://www.geeksforgeeks.org/maximum-sum-pairs-specific-difference/
+ * Given an array of integers and a number k. We can pair two number of array if difference 
+ * between them is strictly less than k. The task is to find maximum possible sum of disjoint pairs.
+ * Sum of P pairs is sum of all 2P numbers of pairs.
  */
 
 /**
